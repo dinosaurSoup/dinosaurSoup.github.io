b47e02da0a15461b9940539bea75dff31434ac1a
dinosaurSoup <gorfmorp@gmail.com>
2026-06-18 16:35:20 +0200

    Updated random page link, added art

--- src/scripts/random-page.js
+++
@@ -1,9 +0,0 @@
-const pages = [
-    "/pages/overworld/000-the-plaza.html"
-];
-
-document.getElementById("random-page").addEventListener("click", () => {
-    var randomIndex = Math.floor(Math.random() * pages.length);
-    var randomPage = pages[randomIndex];
-    window.location.href = randomPage;
-});
